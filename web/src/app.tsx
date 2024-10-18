45f0fcef3d44e42271ab82fa9cf150e1d7966ff7
Mario <me@marioparaschiv.com>
2024-10-18 19:14:07 +0100

    ✨ feat: layout change

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,44 +1,101 @@
 import { ExternalLink } from 'lucide-react';
 import { useMemo, useState } from 'react';
 
-import { useData } from './providers/websocket-provider';
-import CopyField from './components/copy-field';
+import { useData, type EntityUpdate } from './providers/websocket-provider';
+import config from '../../config.json';
 
 
 function App() {
 	const { data, isLoading } = useData();
 	const [tapped, setTapped] = useState(false);
 
-	const sortedData = useMemo(() => Object.entries(data.payload.store).sort(([addressA, updatesA], [addressB, updatesB]) => {
-		const totalBuysA = updatesA.length;
-		const totalBuysB = updatesB.length;
-		return totalBuysB - totalBuysA;
-	}), [data]);
+	const grouped = useMemo(() => {
+		const result: Record<number, Record<string, EntityUpdate[]>> = {};
+		const store = data.payload.store;
+
+		// Iterate over the 'store' object which holds the coin transaction history
+		for (const [address, updates] of Object.entries(store)) {
+			const buys = updates.length;
+
+			if (buys < config.minimumBuys.display) continue;
+
+			result[buys] ??= {};
+			result[buys][address] = updates;
+			// const value = store[coin];
+			// const numBuys = value.length; // Get the number of transactions (buys)
+
+			// // If the number of buys (numBuys) is not yet a key in 'result', initialize it as an array
+			// if (!result[numBuys]) {
+			// 	result[numBuys] = [];
+			// }
+
+			// // Push the coin into the respective group based on the number of buys
+			// result[numBuys].push(coin);
+		}
+
+		return result;
+	}, [data]);
+
+	console.log(grouped);
+
+
+	const sortedData = useMemo(() => Object.entries(grouped).sort(([amountA], [amountB]) => Number(amountB) - Number(amountA)), [grouped]);
 
 	return (
-		<div className='grid p-4 grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-2'>
-			{!tapped ? <button className='bg-neutral-800 border border-neutral-700 font-semibold rounded-md' onClick={() => setTapped(true)}>
-				Tap me to activate TTS
-			</button> : null}
+		<div className='flex flex-col'>
+			{!tapped ? <div className='px-4 py-1 w-full flex flex-col gap-2'>
+				<button className='bg-neutral-800 w-full border border-neutral-700 font-semibold rounded-md' onClick={() => setTapped(true)}>
+					Tap me to activate TTS
+				</button>
+				<div className='w-full h-0.5 bg-neutral-800' />
+			</div> : null}
 			{isLoading && 'Connecting...'}
-			{!isLoading && Object.keys(data.payload.store).length === 0 && 'No purchases have been captured. When one gets captured, it will appear here.'}
-			{Object.keys(data.payload.store).length !== 0 && sortedData.map(([address, updates]) => {
-				return (
-					<div key={address} className='flex p-2 w-full rounded-md bg-neutral-800 border border-neutral-700 items-start overflow-clip'>
-						<div className='flex flex-col overflow-hidden' key={address}>
-							<span>
-								<span className='text-xl font-bold'>{updates.length}</span> buy{updates.length > 1 ? 's' : ''}
-							</span>
-							{data.payload.names[address] && <span className='truncate'>{data.payload.names[address]}</span>}
-							<CopyField className='truncate' value={address} />
-						</div>
-						{data.payload.charts[address] && <a className='ml-auto' target='_blank' href={data.payload.charts[address]} role='button'>
-							<ExternalLink size={18} />
-						</a>}
+			{!isLoading && sortedData.length === 0 && 'No purchases have been captured. When one gets captured, it will appear here.'}
+			{sortedData.map(([amount, coins]) => <>
+				<div key={amount} className='flex gap-6 px-4 py-2 w-full'>
+					<span className='text-2xl font-bold min-w-[50px]'>{amount}</span>
+					<div className='w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-2'>
+						{Object.entries(coins).map(([address, updates]) => {
+							return (
+								<div
+									key={address}
+									role='button'
+									className='flex p-2 w-full rounded-md bg-neutral-800 border border-neutral-700 items-start overflow-clip'
+									onClick={(event) => {
+										const textArea = document.createElement('textarea');
+
+										textArea.style.position = 'fixed';
+										textArea.style.left = '-999999px';
+										textArea.value = address;
+
+										document.body.appendChild(textArea);
+										textArea.focus();
+										textArea.select();
+
+										try {
+											document.execCommand('copy');
+										} catch (err) {
+											console.error('Unable to copy to clipboard', err);
+										} finally {
+											textArea.remove();
+										}
+									}}
+								>
+									<div className='flex flex-col overflow-hidden' key={address}>
+										{data.payload.names[address] && <span className='truncate'>{data.payload.names[address]}</span>}
+									</div>
+									{data.payload.charts[address] && <a className='ml-auto' target='_blank' href={data.payload.charts[address]} role='button'>
+										<ExternalLink size={18} />
+									</a>}
+								</div>
+							);
+						})}
 					</div>
-				);
-			})}
-		</div>
+
+				</div>
+				<div className='w-full h-0.5 bg-neutral-800' />
+			</>)}
+		</div >
 	);
 }
 
