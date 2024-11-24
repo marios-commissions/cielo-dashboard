e5910f38de311d5396981944bf41ec6f01e6719e
Mario <me@marioparaschiv.com>
2024-11-24 10:32:21 +0000

    🐛 fix: strict mode bullshit

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,16 +1,13 @@
 import './index.css';
 
 import { createRoot } from 'react-dom/client';
-import { StrictMode } from 'react';
 
 import DataProvider from './providers/websocket-provider';
 import App from './app';
 
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<DataProvider>
-			<App />
-		</DataProvider>
-	</StrictMode>,
+	<DataProvider>
+		<App />
+	</DataProvider>
 );
